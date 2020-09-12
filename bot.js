17a3d5340e9b2c37acfbbaae8e11c9aad5045e11
YakaryBovine <kaz9876@gmail.com>
2020-09-12 20:16:27 +1200

    sdfdasf

--- bot.js
+++ bot.js
@@ -2,29 +2,17 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
- 
-
 client.on('ready', () => {
-
-    console.log('I am ready!');
-    client.user.setActivity('Awaiting your Elo');
-
+  console.log('I am ready!');
 });
-
- 
 
 client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
 });
 
- 
+
 
 // THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);
